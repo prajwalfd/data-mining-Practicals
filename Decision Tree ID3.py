f94195b8fb5d41be0a71c1f99cbb5cd00a687845
Prajwal Fernandes <55756143+prajwalfd@users.noreply.github.com>
2021-04-08 13:15:22 +0530

    Create Decision Tree ID3.py
    Decision Tree ID3 algorithm.

---
+++ Decision Tree ID3.py
@@ -0,0 +1,87 @@
+import numpy as np
+import yaml
+import pandas as pd
+from numpy import log2 as log
+eps = np.finfo(float).eps
+
+Outlook = 'Overcast,Overcast,Overcast,Overcast,Rainy,Rainy,Rainy,Rainy,Rainy,Sunny,Sunny,Sunny,Sunny,Sunny'.split(',')
+Temp = 'Hot,Cool,Mild,Hot,Mild,Cool,Cool,Mild,Mild,Hot,Hot,Mild,Cool,Mild'.split(',')
+Humidity = 'High,Normal,High,Normal,High,Normal,Normal,Normal,High,High,High,High,Normal,Normal'.split(',')
+Windy = 'Weak,Strong,Strong,Weak,Weak,Weak,Strong,Weak,Strong,Weak,Strong,Weak,Weak,Strong'.split(',')
+Playgame = 'yes,yes,yes,yes,yes,yes,no,yes,no,no,no,no,yes,yes'.split(',')
+
+dataset ={'Outlook':Outlook,'Temp':Temp,'Humidity':Humidity,'Windy':Windy,'Playgame':Playgame}
+df = pd.DataFrame(dataset,columns=['Outlook','Temp','Humidity','Windy','Playgame'])
+print(df)
+
+def find_entropy(df):
+    Class = df.keys()[-1]   #To make the code generic, changing target variable class name
+    entropy = 0
+    values = df[Class].unique()
+    for value in values:
+        fraction = df[Class].value_counts()[value]/len(df[Class])
+        entropy += -fraction*np.log2(fraction)
+    return entropy
+  
+  
+def find_entropy_attribute(df,attribute):
+  Class = df.keys()[-1]   #To make the code generic, changing target variable class name
+  target_variables = df[Class].unique()  #This gives all 'Yes' and 'No'
+  variables = df[attribute].unique()    #This gives different features in that attribute (like 'Hot','Cold' in Temperature)
+  entropy2 = 0
+  for variable in variables:
+      entropy = 0
+      for target_variable in target_variables:
+          num = len(df[attribute][df[attribute]==variable][df[Class] ==target_variable])
+          den = len(df[attribute][df[attribute]==variable])
+          fraction = num/(den+eps)
+          entropy += -fraction*log(fraction+eps)
+      fraction2 = den/len(df)
+      entropy2 += -fraction2*entropy
+  return abs(entropy2)
+
+
+def find_winner(df):
+    IG = []
+    for key in df.keys()[:-1]:
+        IG.append(find_entropy(df)-find_entropy_attribute(df,key))
+    return df.keys()[:-1][np.argmax(IG)]
+  
+  
+def get_subtable(df, node,value):
+  return df[df[node] == value].reset_index(drop=True)
+
+
+def buildTree(df,tree=None): 
+    Class = df.keys()[-1]   #To make the code generic, changing target variable class name
+    
+    #Here we build our decision tree
+
+    #Get attribute with maximum information gain
+    node = find_winner(df)
+    
+    #Get distinct value of that attribute e.g Salary is node and Low,Med and High are values
+    attValue = np.unique(df[node])
+    
+    #Create an empty dictionary to create tree    
+    if tree is None:                    
+        tree={}
+        tree[node] = {}
+    
+    #We make loop to construct a tree by calling this function recursively. 
+    #In this we check if the subset is pure and stops if it is pure. 
+
+    for value in attValue:
+        
+        subtable = get_subtable(df,node,value)
+        clValue,counts = np.unique(subtable[Class],return_counts=True)                        
+        
+        if len(counts)==1:  #Checking purity of subset
+            tree[node][value] = clValue[0]                                                    
+        else:        
+            tree[node][value] = buildTree(subtable) #Calling the function recursively 
+    return tree
+
+t = buildTree(df)
+print("\nDecision Tree\n")
+print(yaml.dump(t, default_flow_style=False).replace('  ','\t'))
